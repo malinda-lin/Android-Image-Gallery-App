085350e7c6de03d944f9e69399365b049af0491a
Xiaoqing-Lin <57362207+Xiaoqing-Lin@users.noreply.github.com>
2021-01-09 18:57:16 -0500

    Start listener for bottomNavbar

--- app/src/main/java/com/onramp/android/takehome/fragments/BottomNavigationBar.kt
+++ app/src/main/java/com/onramp/android/takehome/fragments/BottomNavigationBar.kt
@@ -1,21 +1,40 @@
 package com.onramp.android.takehome.fragments
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.onramp.android.takehome.R
 
 class BottomNavigationBar : Fragment(R.layout.fragment_bottom_nav_bar) {
 
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//    }
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-//                              savedInstanceState: Bundle?): View? {
-//        // Inflate the layout for this fragment
-//        return inflater.inflate(R.layout.fragment_bottom_nav_bar, container, false)
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottomNavigation)
+        bottomNavigationView.setOnNavigationItemSelectedListener { item -> setNavigationButtonListener(item)}
+    }
+
+    private fun setNavigationButtonListener(item: MenuItem) : Boolean {
+        return when(item.itemId) {
+            R.id.exploreNavigation -> {
+                Log.d("mylog", "explore!")
+                true
+            }
+            R.id.searchNavigation -> {
+                Log.d("mylog", "search!")
+                true
+            }
+            R.id.favoriteNavigation -> {
+                Log.d("mylog", "favorite!")
+                true
+            }
+            else -> false
+        }
+    }
 
 }
